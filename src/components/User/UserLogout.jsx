8011f455ded0cfa41d99afaca2e0a922c8c8261a
Framzy <fardenramzy97@gmail.com>
2025-10-20 13:29:06 +0700

    Create logout api in user api then create function logout and remove token from local storage after logout

---
+++ src/components/User/UserLogout.jsx
@@ -0,0 +1,30 @@
+import { useEffectOnce, useLocalStorage } from "react-use";
+import { userLogout } from "../../lib/api/UserApi";
+import { alertError, alertSuccess } from "../../lib/alert";
+import { useNavigate } from "react-router";
+
+export default function UserLogout() {
+  const [token, setToken] = useLocalStorage("token", "");
+  const navigate = useNavigate();
+
+  async function handleLogout() {
+    const response = await userLogout({ token });
+    const responseBody = await response.json();
+    console.log(responseBody);
+
+    if (response.status === 200) {
+      setToken("");
+      await alertSuccess("User logged out successfully");
+      console.log(responseBody.data);
+      await navigate({
+        pathname: "/login",
+      });
+    } else {
+      await alertError(responseBody.errors);
+    }
+  }
+
+  useEffectOnce(() => {
+    handleLogout();
+  }, []);
+}
